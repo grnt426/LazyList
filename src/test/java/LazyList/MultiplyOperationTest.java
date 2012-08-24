13bb6ad0ecf9c5b9927237e685ee61b62cc6ad6c
Grant Kurtz <th3guys2@gmail.com>
2012-08-23 21:34:42 -0700

    Added tests specific to MutliplyOperations.

---
+++ src/test/java/LazyList/MultiplyOperationTest.java
@@ -0,0 +1,23 @@
+package LazyList;
+
+import junit.framework.TestCase;
+
+/**
+ * Author:      Grant Kurtz
+ */
+public class MultiplyOperationTest extends TestCase {
+
+	private MultiplyOperation mo;
+
+	private final int product = 9;
+	private final int mutliplier = 3;
+	private final int lhs = 3;
+
+	public void setUp() throws Exception {
+		mo = new MultiplyOperation(mutliplier);
+	}
+
+	public void testEvaluate() throws Exception {
+		assertEquals(product, mo.evaluate(lhs));
+	}
+}
